2ec322ca299684bae6088c4ce100f7759aef4cf8
shahin2525 <shahin451999@gmail.com>
2024-08-23 17:40:59 -0700

    add router

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <h1> i am login</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/routes/router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../root/Root";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import ErrorPage from "../pages/Error/ErrorPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
